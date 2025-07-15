6bcbf2ba41235d51bdd06cb43ef16d3edad9c987
tmtuan684 <tuan.tran24@rmit.edu.vn>
2025-07-15 23:45:27 +0700

    minor edit

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const port = 3000;
 
 // Set EJS as the template engine
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname) ,'./views');
+app.set('views',path.join(__dirname,'views'));
 
 // Middleware to serve static files from the 'public' directory
 app.use(express.static('public'));
